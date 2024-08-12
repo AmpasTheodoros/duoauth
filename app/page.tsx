0931eafaee411dbabfd711778b3d2966242ad405
Theodor Ampas <71594408+AmpasTheodoros@users.noreply.github.com>
2024-08-12 19:46:13 +0300

    new buttons

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,23 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <h2>Hello</h2>
+    <div>
+      <Button variant="locked">locked</Button>
+      <Button variant="default">default</Button>
+      <Button variant="primary">primary</Button>
+      <Button variant="primaryOutline">primaryOutline</Button>
+      <Button variant="secondary">secondary</Button>
+      <Button variant="secondaryOutline">secondaryOutline</Button>
+      <Button variant="danger">danger</Button>
+      <Button variant="dangerOutline">dangerOutline</Button>
+      <Button variant="super">super</Button>
+      <Button variant="superOutline">superOutline</Button>
+      <Button variant="ghost">ghost</Button>
+      <Button variant="sidebar">sidebar</Button>
+      <Button variant="sidebarOutline">sidebarOutline</Button>
+    </div>
+
   );
 }
