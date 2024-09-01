7ae88faf70ffd5c9fafa79047429066fadc1fb56
Theodor Ampas <71594408+AmpasTheodoros@users.noreply.github.com>
2024-09-02 01:32:04 +0300

    rebranding

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -21,7 +21,7 @@ const Sidebar = ({ className }: SidebarProps) => {
         <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
           <Image src={"/mascot.svg"} height={40} width={40} alt={"mascot"} />
           <h1 className="text-2xl font-extrabold text-green-600 tracking-wide">
-            Lingo
+            Crugs
           </h1>
         </div>
       </Link>
